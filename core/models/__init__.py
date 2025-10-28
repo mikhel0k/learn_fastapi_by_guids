2a70ef386076e213742326eb36523830d584a976
Mikhel Lev <levmihel150@gmail.com>
2025-10-28 12:35:24 +0700

    Create users table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,11 +1,13 @@
 __all__ = {
     'Base',
-    'Product',
     'DataBaseHelper',
     'db_helper',
+    'Product',
+    'User',
 }
 
 
 from .db_helper import db_helper, DataBaseHelper
 from .base import Base
 from .product import Product
+from .user import User
